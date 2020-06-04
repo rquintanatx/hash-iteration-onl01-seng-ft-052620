970ec5292e556699742d582f46d263274f0bef81
Robert Quintana <robertquintanatx@gmail.com>
2020-06-04 07:17:43 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,9 +6,15 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |name, age|
-    
       puts "Happy Birthday #{name}! You are now #{age} years old!"
-  
+  end
+end
+
+def age_appropiate_birthday(birthday_kids)
+  birthday_kids.each do |name, age|
+    if age <= 12
+      puts"Happy Birthday #{name}! You are now #{age} years old!"
+    end
   end
 end
 
